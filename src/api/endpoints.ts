6bc35841d8e90ba995addbe69e87c3c119514175
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-01-31 22:53:02 +0545

    Add missing API endpoint constants for friend-related actions
    - Defined constants for the following endpoints:
      - ADD_FRIEND_REQUEST_ENDPOINT_POST for adding friend requests.
      - PENDING_FRIENDS_REQUEST_ENDPOINT_GET for retrieving pending friend requests.
      - FRIEND_REQUEST_ACCEPT_ENDPOINT_POST for accepting friend requests.

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,6 +3,10 @@ export const REGISTER_ENDPOINT_POST = "/api/auth/register";
 export const REFRESH_TOKEN_ENDPOINT_POST = "";
 export const FIND_FRIENDS_ENDPOINT_POST = "/api/social/friends-search";
 export const SILENT_LOGIN_ENDPOINT_POST = "/api/auth/silent-login";
+export const ADD_FRIEND_REQUEST_ENDPOINT_POST = "/api/social/add-friend-request";
+export const PENDING_FRIENDS_REQUEST_ENDPOINT_GET = "/api/social/pending-friends-request";
+export const FRIEND_REQUEST_ACCEPT_ENDPOINT_POST = "/api/social/friend-request-accept";
+
 // [
 //     {
 //       "endpoint": "/api/auth/register",
