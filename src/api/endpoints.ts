1a888e494daa0685d18a8cad7c71d211fee2711d
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-02-05 13:21:51 +0545

    Modified route path

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -11,34 +11,19 @@ export const ACCEPTED_FRIEND_REQUESTS_ENDPOINT_POST =
   "/api/social/accepted-friend-request";
 export const FRIEND_REQUEST_ACCEPT_ENDPOINT_POST =
   "/api/social/friend-request-accept";
+export const CANCEL_OR_REJECT_FRIEND_REQUEST =
+  "/api/social/pending-request-reject";
 
-// [
-//     {
-//       "endpoint": "/api/auth/register",
-//       "method": "POST"
-//     },
-//     {
-//       "endpoint": "/api/auth/login",
-//       "method": "POST"
-//     },
-//     {
-//       "endpoint": "/api/silent/login",
-//       "method": "POST"
-//     },
-//     {
-//       "endpoint": "/api/social/friends-search",
-//       "method": "POST"
-//     },
-//     {
-//       "endpoint": "/api/social/add-friend-request",
-//       "method": "POST"
-//     },
-//     {
-//       "endpoint": "/api/social/pending-friends-request",
-//       "method": "GET"
-//     },
-//     {
-//       "endpoint": "/api/social/friend-request-accept",
-//       "method": "POST"
-//     }
-//   ]
+export const API_ROOT_GET = "/api";
+
+export const AUTH_REGISTER_POST = "/auth/register";
+export const AUTH_LOGIN_POST = "/auth/login";
+
+export const SOCIAL_FRIENDS_SEARCH_POST = "/social/friends/search";
+export const SOCIAL_FRIEND_REQUEST_POST = "/social/friends/requests";
+export const SOCIAL_PENDING_REQUESTS_GET = "/social/friends/requests/pending";
+export const SOCIAL_ACCEPTED_REQUESTS_GET = "/social/friends/requests/accepted";
+export const SOCIAL_ACCEPT_REQUEST_POST = "/social/friends/requests/accept";
+export const SOCIAL_REJECT_REQUEST_POST = "/social/friends/requests/reject";
+
+export const SILENT_LOGIN_POST = "/silent/login";
