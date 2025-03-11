29a5aa406727272008a4fc5538cc38b846e40836
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-03-11 17:11:14 +0545

    Update navigation: -  Update navigation routes based on whether the user profile is set up or not

--- src/hooks/auth/useReauthorize.ts
+++ src/hooks/auth/useReauthorize.ts
@@ -1,21 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuthStore } from "../../store/authStore";
 import { useNavigate } from "react-router-dom";
-import { HOME_ROUTE, LOGIN_ROUTE } from "../../router/routerPath";
+import {
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  PREF_PROFILE_SETUP,
+} from "../../router/routerPath";
 import { SocketSingleton } from "../../socket/socket";
+import { ACCESS_TOKEN } from "../../api/constants";
 
 const useAuthReauthorize = () => {
   const attemptAuthorization = useAuthStore(
     (state) => state.attemptAuthorization
   );
   const navigate = useNavigate();
+  const isProfileSetup = useRef(false);
 
   useEffect(() => {
     const reAuthorize = async () => {
+      if (!localStorage.getItem(ACCESS_TOKEN)) return;
       const res = await attemptAuthorization();
       if (res && res.status === 200) {
         SocketSingleton.connectSocket();
-        navigate(HOME_ROUTE);
+        const navRoute = isProfileSetup.current
+          ? HOME_ROUTE
+          : PREF_PROFILE_SETUP;
+        navigate(navRoute);
       } else {
         navigate(LOGIN_ROUTE);
       }
