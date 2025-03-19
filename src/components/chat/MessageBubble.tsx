6d875420c0bf3c6b6f2395d78c1183dac7316d6c
Bishnu Chalise <cryptographycipher29@gmail.com>
2025-03-19 20:41:43 +0545

    Update(ui):  - Remove bg color from image file and include in message only

--- src/components/chat/MessageBubble.tsx
+++ src/components/chat/MessageBubble.tsx
@@ -16,22 +16,22 @@ const MessageBubble: FC<MessageBubbleProps> = ({
 }) => {
   const setMediaUrl = useMediaPreviewStore((state) => state.setMediaUrl);
 
+  const bubbleStyles = isFile
+    ? "bg-transparent"
+    : isUser
+    ? "bg-gradient-to-r from-blue-500 to-blue-400 text-white"
+    : "bg-gray-100 text-gray-900";
+
+  const textOpacity = isLast ? "opacity-20" : "";
+
   return (
     <div className={`flex ${isUser ? "justify-end" : "justify-start"} my-1`}>
       <div
-        className={`relative p-3 px-4 max-w-[80%] md:max-w-[65%] lg:max-w-[50%] rounded-2xl shadow-md
-          ${
-            isUser
-              ? "bg-gradient-to-r from-blue-500 to-blue-400 text-white"
-              : "bg-gray-100 text-gray-900"
-          }
-        `}
+        className={`relative p-3 px-4 max-w-[80%] md:max-w-[65%] lg:max-w-[50%] rounded-2xl shadow-md ${bubbleStyles}`}
       >
         {!isFile ? (
           <p
-            className={`text-md font-poppins leading-snug break-words ${
-              isLast ? "opacity-20" : ""
-            }`}
+            className={`text-md font-poppins leading-snug break-words ${textOpacity}`}
           >
             {message}
           </p>
